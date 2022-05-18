019903fcec6ca4b70e7f73c99315f9b6b1689381
liliquan <212320016@hdu.edu.cn>
2022-05-18 19:13:45 +0800

    Initial commit

---
+++ expression_tree_test.py
@@ -0,0 +1,17 @@
+import unittest
+from math import *
+from hypothesis import given
+
+import hypothesis.strategies as st
+
+from expression_tree import *
+
+
+class TestExpressTree(unittest.TestCase):
+
+    def test_example(self):
+        sting = 'a + 2 - sin(-0.3)*(b - c)'
+        fun = ExpressTree(sting, sin=lambda a: sin(a))
+        # fun.visualization()
+        t0 = 4 + 2 - sin(-0.3) * (2 - 66)
+        self.assertEqual(t0, fun(a=4, b=2, c=66))
