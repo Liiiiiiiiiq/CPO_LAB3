ed02825950363d09b5bd6df9d028629331a076b2
liliquan <212320016@hdu.edu.cn>
2022-05-18 19:36:44 +0800

    update

--- expression_tree_test.py
+++ expression_tree_test.py
@@ -12,6 +12,6 @@ class TestExpressTree(unittest.TestCase):
     def test_example(self):
         sting = 'a + 2 - sin(-0.3)*(b - c)'
         fun = ExpressTree(sting, sin=lambda a: sin(a))
-        # fun.visualization()
+        fun.visualization()
         t0 = 4 + 2 - sin(-0.3) * (2 - 66)
         self.assertEqual(t0, fun(a=4, b=2, c=66))
